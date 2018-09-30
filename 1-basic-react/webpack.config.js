05b1562a488e836d2b56807c84abf7bbbcabf668
gerkenv <gerkenv@gmail.com>
2018-10-01 01:59:35 +0200

    Initial project structure is prepared;

--- 1-basic-react/webpack.config.js
+++ 1-basic-react/webpack.config.js
@@ -1,18 +1,39 @@
-var debug = process.env.NODE_ENV || "development"; // "production";
+var deploymentMode = process.env.NODE_ENV || "development"; // "production" 
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-    context: __dirname,
-    mode: debug,
-    devtool: debug ? "inline-sourcemap" : null,
-    entry: "./js/script.js",
-    output: {
-        path: __dirname + "/js",
-        filename: "script.min.js"
-    },
-    plugins: debug ? [] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
+  context: path.join(__dirname, "src"),
+  devtool: "inline-sourcemap",
+  mode: deploymentMode,
+  entry: "./js/client.js",
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: [
+            'react-html-attrs', 
+            ["@babel/plugin-proposal-decorators", { "legacy": true }],
+            'transform-class-properties'
+          ],
+        }
+      }
     ]
+  },
+  output: {
+    path: __dirname + "/dist/",
+    filename: "client.min.js"
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: deploymentMode === "development" ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
 };
