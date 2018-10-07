240d3fc90584f85f80cbcfc4ebe1bc5120a630fa
gerkenv <gerkenv@gmail.com>
2018-10-07 18:06:13 +0200

    no actual changes here;

--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -9,13 +9,14 @@ export default class Layout extends React.Component {
   }
 
   changeTitle(title) {
-    this.setState({title}); 
+    this.setState({title});
   }
 
   render() {
     return (
       <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
+        <Header changeTitle={this.changeTitle.bind(this)}
+                title={this.state.title}/>
         <Footer/>
       </div>
     );
