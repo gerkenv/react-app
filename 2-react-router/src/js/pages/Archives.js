384a3070dc8e394cadf588b1031df91fbaf2adfd
gerkenv <gerkenv@gmail.com>
2018-10-09 21:52:43 +0200

    Now `Archives` component has sub-parameter and qeury parameters;

--- 2-react-router/src/js/pages/Archives.js
+++ 2-react-router/src/js/pages/Archives.js
@@ -1,9 +1,26 @@
 import React from "react";
+const queryString = require('query-string');
 
 export default class Archives extends React.Component {
+  getAllQueryParams() {
+    const queryParams = queryString.parse(this.props.location.search);
+
+    let params = "";
+    for (let param in queryParams) {
+      params += " / " + param + " is " + queryParams[param];
+    }
+    return params;
+  }
+
   render() {
+    console.log(this.props);
+    const { article } = this.props.match.params;
+    let params = this.getAllQueryParams();
     return (
-      <h1>Archives</h1>
+      <div>
+        <h1>Archives / {article}</h1>
+        <h2>{params}</h2>
+      </div>
     );
   }
 }
