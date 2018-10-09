32505a0147e39dbc455f74381e2fa43d8bcb7da6
gerkenv <gerkenv@gmail.com>
2018-10-09 02:04:23 +0200

    New routes are set up;

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -7,11 +7,23 @@ import Bootstrap from "./vendor/bootstrap-without-jquery";
 
 import Layout from "./pages/Layout";
 
+import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
+import Settings from "./pages/Settings";
+
 const app = document.getElementById('app');
 
 ReactDOM.render(
   <HashRouter>
-    <Route path="/" component={Layout}></Route>
+    <div>
+      {/* If we remove `exact` then `Layout` will be displayed
+      at any route that begins with `/` */}
+      <Route exact path="/" component={Layout}/>
+      {/* Beware of hash sign `#` - `localhost:8080/#/archives` */}
+      <Route path="/archives" component={Archives}/>  
+      <Route path="/settings" component={Settings}/>
+      <Route path="/featured" component={Featured}/>
+    </div>
   </HashRouter>,
   app
 );
