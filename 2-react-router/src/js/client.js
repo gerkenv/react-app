5509012fb440c56ed24d5c16c27dfece3605d1cb
gerkenv <gerkenv@gmail.com>
2018-10-10 15:52:35 +0200

    Client.js is minimised - routing is moved to `Layout`;

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -1,30 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route } from "react-router";
-import { HashRouter } from "react-router-dom";
-
-import Bootstrap from "./vendor/bootstrap-without-jquery";
 
 import Layout from "./pages/Layout";
 
-import Archives from "./pages/Archives";
-import Featured from "./pages/Featured";
-import Settings from "./pages/Settings";
-
 const app = document.getElementById('app');
 
-ReactDOM.render(
-  <HashRouter>
-    <div>
-      {/* If we use `exact path` then component is displayed
-      only at `/` route, but if we use `path` then component
-      is displayed at every route that begins with `/` */}
-      <Route path="/" component={Layout}/>
-      {/* Beware of hash sign `#` - `localhost:8080/#/archives` */}
-      <Route path="/archives/:article?" component={Archives}/>
-      <Route path="/settings" component={Settings}/>
-      <Route exact path="/" component={Featured}/>
-    </div>
-  </HashRouter>,
-  app
-);
+ReactDOM.render(<Layout/>, app);
